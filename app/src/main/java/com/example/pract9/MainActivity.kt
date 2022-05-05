9a6bf01a631815554d294d28c0e457f67bd11ce5
jora132 <ko4nevg@yadex.ru>
2022-05-05 15:20:44 +0700

    Почти начало

---
+++ app/src/main/java/com/example/pract9/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pract9
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
